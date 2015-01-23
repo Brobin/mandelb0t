1015c2429af0ebb0b254c09ccb117596ad97ab33
Tobin <tobinjbrown@gmail.com>
2015-01-22 19:35:09 -0600

    refactored to a class based structure

--- colors.py
+++ colors.py
@@ -1,51 +1,58 @@
 import random
 
-def random_pastel_rgb():
+class ColorGenerator():
     """
-    Generates an array of random RGB colors based on a random
-    seed color. First it generates a rnadom color, then
-    calls random_seed_rgb to get a set based on that.
+    This class is used to generate and array of rgb color combinations
+    It contains methods that allow it to do so completely randomly,
+    based on a random color, or on a provided color.
     """
-    rgb = []
-    mix = []
-    mix.append(random.randint(0,256))
-    mix.append(random.randint(0,256))
-    mix.append(random.randint(0,256))
-    return random_seeded_rgb(mix)
 
-def random_bright_rgb():
-    """
-    Generates a completely random set of colors. The nature of
-    the randomness generally makes it so that the colors
-    are very bright, obnoxious and non-matching.
-    """
-    rgb = []
-    for x in range(0,64):
-        r = random.randint(0,256)
-        g = random.randint(0,256)
-        b = random.randint(0,256)
-        rgb.append([r,g,b])
-    return rgb
+    def __init__(self, elements=64, seed=None):
+        self.elements = elements
+        if seed is None:
+            seed = self.generate_seed()
+        self.seed = seed
 
-def random_seeded_rgb(seed):
-    """
-    Generates a set of colors based on a passed in color.
-    Half random, half seed. Alternates light and dark
-    shades to make layers more distinguishable.
-    """
-    rgb = []
-    dark = True
-    for x in range(0,64):
-        if dark:
-            r = (random.randint(0,128) + seed[0]) / 2
-            g = (random.randint(0,128) + seed[1]) / 2
-            b = (random.randint(0,128) + seed[2]) / 2
+    def generate_seed(self):
+        """
+        Generates an seed color to be used to generate the 
+        set of colors. It does this by randomly choosing 
+        each part of the RGB set.
+        """
+        self.seed = []
+        self.seed.append(random.randint(0,256))
+        self.seed.append(random.randint(0,256))
+        self.seed.append(random.randint(0,256))
+        return self.seed
+
+    def random_rgb(self):
+        """
+        Generates a completely random set of colors. The nature of
+        the randomness generally makes it so that the colors
+        are very bright, obnoxious and non-matching.
+        """
+        rgb = []
+        for x in range(0,self.elements):
+            r = random.randint(0,256)
+            g = random.randint(0,256)
+            b = random.randint(0,256)
             rgb.append([r,g,b])
-            dark = False
-        else:
-            r = (random.randint(128,256) + seed[0]) / 2
-            g = (random.randint(128,256) + seed[1]) / 2
-            b = (random.randint(128,256) + seed[2]) / 2
+        return rgb
+
+    def random_seeded_rgb(self):
+        """
+        Generates a set of colors based on a passed in color.
+        Half random, half seed. Alternates light and dark
+        shades to make layers more distinguishable.
+        """
+        rgb = []
+        dark = True
+        for x in range(0,self.elements):
+            min = (0 if dark else 128)
+            max = (128 if dark else 256)
+            dark = (False if dark else True)
+            r = (random.randint(min, max) + self.seed[0]) / 2
+            g = (random.randint(min, max) + self.seed[1]) / 2
+            b = (random.randint(min, max) + self.seed[2]) / 2
             rgb.append([r,g,b])
-            dark = True
-    return [rgb, seed]
+        return rgb
--- mandelbot.py
+++ mandelbot.py
@@ -7,80 +7,82 @@ import random
 import os
 from settings import *
 from local_settings import *
-from colors import *
-
-"""
-This script creates an image of the mandelbrot set with
-a random color scheme and posts it to Twitter.
-
-2014
-Tobin Brown
-"""
+from colors import ColorGenerator
 
 
-#Make the OAuth connection
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.secure = True
-auth.set_access_token(access_token, access_token_secret)
-
-#Connect to the API
-api = tweepy.API(auth)
-
-def mandelbrot():
+class MandelBot():
     """
-    Creates an image of the mandelbrot set and saves it to
-    a file.
-        1. Runs the algorithm to generate the image.
-        2. Calls the rbg functions to get the color schemes.
-        3. Applies colors to each layer
-        4. Saves the image
-        5. Returns the absolute file path of the image
+    This script creates an image of the mandelbrot set with
+    a random color scheme and posts it to Twitter.
+
+    2014
+    Tobin Brown
     """
-    xa = -2.0
-    xb = 1.0
-    ya = -1.5
-    yb = 1.5
-    max_iterations = 256
-    size = 512
 
-    #Create a new image
-    image = Image.new("RGB", (size, size))
-    mtx = image.load()
+    def __init__(self):
+        #Make the OAuth connection
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.secure = True
+        auth.set_access_token(access_token, access_token_secret)
 
-    lutx = [j * (xb-xa) / (size - 1) + xa for j in xrange(size)]
+        #Connect to the API
+        self.api = tweepy.API(auth)
 
-    #Get the random rgb values for coloring
-    color_set = random_pastel_rgb()
-    rgb = color_set[0]
-    seed = color_set[1]
+        #Get the random rgb values for coloring
+        self.generator = ColorGenerator(64)
+        self.rgb = self.generator.random_seeded_rgb()
+        self.seed = self.generator.seed
 
-    #Create the mandelbrot set
-    for y in xrange(size):
-        cy = y * (yb - ya) / (size - 1)  + ya
-        for x in xrange(size):
-            c = complex(lutx[x], cy)
-            z = 0
-            for i in xrange(max_iterations):
-                if abs(z) > 2.0: break 
-                z = z * z + c
-            #Color the layer i
-            r = rgb[i%64][0]
-            g = rgb[i%64][1]
-            b = rgb[i%64][2]
-            mtx[x, y] =  r,g,b
+    def generate_image(self):
+        """
+        Creates an image of the mandelbrot set and saves it to a
+        file. Runs the algorithm to generate the set, applies
+        the colors to the image, and saves the image
+        """
+        xa = -2.0
+        xb = 1.0
+        ya = -1.5
+        yb = 1.5
+        max_iterations = 256
+        size = 512
 
-    #Save the image
-    millis = int(round(time.time() * 1000))
-    directory = os.path.dirname(os.path.realpath(__file__))
+        #Create a new image
+        image = Image.new("RGB", (size, size))
+        mtx = image.load()
 
-    name = "{0}/images/mandelb0t_{1}.png".format(directory, millis)
-    image.save(name, "PNG")
+        lutx = [j * (xb-xa) / (size - 1) + xa for j in xrange(size)]
 
-    return [name, seed]
+        #Create the mandelbrot set
+        for y in xrange(size):
+            cy = y * (yb - ya) / (size - 1)  + ya
+            for x in xrange(size):
+                c = complex(lutx[x], cy)
+                z = 0
+                for i in xrange(max_iterations):
+                    if abs(z) > 2.0: break 
+                    z = z * z + c
+                #Color the layer i
+                r = self.rgb[i%64][0]
+                g = self.rgb[i%64][1]
+                b = self.rgb[i%64][2]
+                mtx[x, y] =  r,g,b
 
-result = mandelbrot()
-image = result[0]
-seed = result[1]
+        #Save the image
+        name = self.image_name()
+        image.save(name, "PNG")
+        return self.name
 
-#Tweet the status with the image
-api.update_with_media(image, status=str(seed))
+    def image_name(self):
+        millis = int(round(time.time() * 1000))
+        directory = os.path.dirname(os.path.realpath(__file__))
+        self.name = "{0}/images/mandelb0t_{1}.png".format(directory, millis)
+        return self.name
+
+    def send_tweet(self):
+        #Tweet the status with the image
+        self.api.update_with_media(image, status=str(self.seed))
+
+
+bot = MandelBot()
+image = bot.generate_image()
+bot.send_tweet()
